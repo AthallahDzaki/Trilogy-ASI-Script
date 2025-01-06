b229050bf2c28289be0fefecea6ed3908512ce62
Athallah Dzaki <athallahdzaki@gmail.com>
2025-01-06 14:57:05 +0700

    Fix SDK Error

--- cmake/pluginSDK.cmake
+++ cmake/pluginSDK.cmake
@@ -7,3 +7,7 @@ set(PSDK_BUILD_SA ON)
 
 message("Fetching plugin-sdk from GitHub...")
 FetchContent_MakeAvailable(plugin-sdk)
+
+# Set PLUGIN_SDK_DIR to the location of the downloaded plugin-sdk
+set(PLUGIN_SDK_DIR ${plugin-sdk_SOURCE_DIR})
+message("PLUGIN_SDK_DIR set to ${PLUGIN_SDK_DIR}")
